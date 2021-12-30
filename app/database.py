c4b5498ec088fcbc60e1d67b9f68267d899b36b2
Max Tang <maxtang.312@gmail.com>
2021-12-30 12:38:09 -0800

    updated javascript files and routes.py file

--- app/database.py
+++ app/database.py
@@ -1,5 +1,16 @@
-import random
 
-def todo_items():
-    todo = [{"num": 1, "task": "Task", "status": "In Progress"}, {"num": 2, "task": "Task", "status": "Complete"}]
+
+def todo_items()->dict:
+    todo = [{"id": 1, "task": "Task", "status": "In Progress"}, {"id": 2, "task": "Task", "status": "Complete"}]
     return todo
+
+def update_task_entry(task_id: int, text: str)->None:
+    pass
+
+def update_status_entry(task_id: int, text: str)->None:
+    pass
+def insert_new_task(text: str)-> int:
+    pass
+    return 1
+def remove_task_by_id(task_id: int)->None:
+    pass
